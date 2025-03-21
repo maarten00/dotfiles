2d9bb937f717149e7a620bcfb434cce4fe75f707
Maarten Kuiper <m.kuiper@exonet.nl>
2025-03-21 10:01:08 +0100

    Add aliases for xdebug on/off on devbox

--- aliases.zsh
+++ aliases.zsh
@@ -3,6 +3,8 @@ alias reload="omz reload"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+alias xo='xdebug.sh on'
+alias xx='xdebug.sh on'
 
 # Directories
 alias dotfiles="cd $DOTFILES"
